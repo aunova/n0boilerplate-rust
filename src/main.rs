1a3b995636f61f12bb15ffdf2b90cfddded44485
Tristram Oaten <tris@oat.sh>
2024-01-23 17:23:59 +0000

    Add some sensible defaults to main.rs
    This looks great! Here's some sensible defaults for your main.rs (others can likely improve on this too!)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
+use color_eyre::eyre::Result;
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
     println!("Hello, world!");
+    Ok(())
+}
+
+// #[cfg(test)] omitted because we want test linting
+mod tests {
+    #![allow(clippy::used_underscore_binding)]
+
+    #[allow(clippy::wildcard_imports)]
+    use rstest::*;
+
+    #[fixture]
+    #[once]
+    fn setup() {
+        #[allow(clippy::unwrap_used)]
+        color_eyre::install().unwrap();
+        // Add any other setup here
+
+    }
+
+    #[rstest]
+    fn demo_failing_test(_setup: ()) {
+        panic!("This demos a failing test");
+    }
 }
